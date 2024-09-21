5f502846002494f31c78f007a42d471d37d07c41
Danillo <danillordm@gmail.com>
2024-09-21 19:43:11 -0300

    Add db_connect

--- password_manager.py
+++ password_manager.py
@@ -1,17 +1,25 @@
 import sqlite3
+from unittest.mock import patch
 from encryptor import Encryptor, load_key
+from os import path, getcwd, makedirs
 
 
 class PasswordManager:
-    def __init__(self, db_path="database/password_db.sqlite"):
-        self.conn = sqlite3.connect(db_path)
+    def __init__(self, db_path="database/password_db.sqlite") -> None:
+        self.conn: sqlite3.Connection = self.db_connect(db_path)
         self.create_table()
 
         # Carregar a chave e inicializar o criptografador
-        self.key = load_key()
+        self.key: bytes = load_key()
         self.encryptor = Encryptor(self.key)
 
-    def create_table(self):
+    def db_connect(self, db_path) -> sqlite3.Connection:
+        patch = path.join(getcwd(), "database")
+        if not path.exists(patch):
+            makedirs(patch)
+        return sqlite3.connect(db_path)
+
+    def create_table(self) -> None:
         query = """CREATE TABLE IF NOT EXISTS passwords (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         category TEXT NOT NULL,
@@ -22,7 +30,7 @@ class PasswordManager:
         self.conn.execute(query)
         self.conn.commit()
 
-    def add_password(self, category, description, login, plain_password):
+    def add_password(self, category, description, login, plain_password) -> None:
         """Adiciona uma nova senha criptografada ao banco de dados."""
         encrypted_password = self.encryptor.encrypt(plain_password)
         query = "INSERT INTO passwords (category, description, login, encrypted_password) VALUES (?, ?, ?, ?);"
@@ -46,7 +54,7 @@ class PasswordManager:
             }
         return None
 
-    def close(self):
+    def close(self) -> None:
         self.conn.close()
 
 
@@ -56,14 +64,14 @@ if __name__ == "__main__":
     print("Banco de dados configurado com sucesso!")
 
     # Exemplo de como adicionar uma senha criptografada
-    manager.add_password(
-        "Social", "Minha conta do Facebook", "meu_login", "minha_senha123"
-    )
-    print("Senha adicionada com sucesso!")
+    # manager.add_password(
+    #     "Social", "Minha conta do Facebook", "meu_login", "minha_senha123"
+    # )
+    # print("Senha adicionada com sucesso!")
 
-    # Exemplo de como consultar e descriptografar a senha
-    senha = manager.get_password(1)
-    if senha:
-        print("Dados recuperados:", senha)
+    # # Exemplo de como consultar e descriptografar a senha
+    # senha = manager.get_password(1)
+    # if senha:
+    #     print("Dados recuperados:", senha)
 
     manager.close()
