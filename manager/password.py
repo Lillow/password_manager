4a00f637642aac608c72a6b07520dd8d8f24d836
Danillo <danillordm@gmail.com>
2024-09-22 18:02:37 -0300

    Add PasswordFilter in password.py

--- manager/password.py
+++ manager/password.py
@@ -1,6 +1,13 @@
 class Password:
-    def __init__(self, category: str, description: str, login: str, password: str) -> None:
-        self.__id: str = None
+    def __init__(
+        self,
+        category: str,
+        description: str,
+        login: str,
+        password: str,
+        id=None,
+    ) -> None:
+        self.__id: str = id
         self.__category: str = category
         self.__description: str = description
         self.__login: str = login
@@ -25,3 +32,32 @@ class Password:
     @property
     def password(self) -> str:
         return self.__password
+
+
+class PasswordFilter(Password):
+    def __init__(
+        self,
+        id: str = None,
+        category: str = None,
+        description: str = None,
+        login: str = None,
+        password: str = None,
+    ) -> None:
+        super().__init__(category, description, login, password)
+
+    @Password.id.setter
+    def id(self, new_id: str):
+        # Modificando diretamente o atributo privado usando name mangling
+        self._Password__id: str = new_id
+
+    @Password.category.setter
+    def category(self, new_category: str):
+        self._Password__category: str = new_category
+
+    @Password.description.setter
+    def description(self, new_description: str):
+        self._Password__description: str = new_description
+
+    @Password.login.setter
+    def login(self, new_login: str):
+        self._Password__login: str = new_login
